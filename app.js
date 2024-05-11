586cd6a6b0d7997749a43e3ae7e1504909d0f5b0
charlien <charlien.rabanal@gmail.com>
2024-05-11 15:11:38 -0400

    comentarios de lo que hace linea de codigo para usar el carrito de compras

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
 //Variables
-const carrito = document.querySelector('#carrito');
-const listaCursos = document.querySelector('#lista-cursos');
-const contenedorCarrito = document.querySelector('#lista-carrito tbody');
-const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
-let articulosCarrito = [];
+const carrito = document.querySelector('#carrito'); /**/ // Selecciona el elemento con el ID "carrito" en el HTML
+const listaCursos = document.querySelector('#lista-cursos'); /**/ // Selecciona el elemento con el ID "lista-cursos" en el HTML
+const contenedorCarrito = document.querySelector('#lista-carrito tbody'); /**/ // Selecciona el tbody dentro del elemento con el ID "lista-carrito" en el HTML
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito'); /**/ // Selecciona el elemento con el ID "vaciar-carrito" en el HTML
+let articulosCarrito = []; /**/ // Crea una variable para almacenar los artículos del carrito
 
 // *****
-cargarEventListeners();
+cargarEventListeners(); /**/ // Llama a la función cargarEventListeners al cargar el script
 
-
-function cargarEventListeners () {
+function cargarEventListeners() {
     //Cuando agregas un curso presionando 'Agregar al carrito'
-    listaCursos.addEventListener('click', agregarCurso);
+    listaCursos.addEventListener('click', agregarCurso); /**/ // Escucha el evento click en el elemento con el ID "lista-cursos" y llama a la función agregarCurso
+
     //Elimina cursos del carrito
-    carrito.addEventListener("click",eliminarCurso);
+    carrito.addEventListener("click", eliminarCurso); /**/ // Escucha el evento click en el elemento con el ID "carrito" y llama a la función eliminarCurso
 
     //muestra los cursos
     document.addEventListener('DOMContentLoaded', () => {
         //recuerda si no hay productos en el carrito se agrega un array vácio para que no de error.
-        articulosCarrito = JSON.parse(localStorage.getItem("carrito")) || [];
-        carritoHTML();
+        articulosCarrito = JSON.parse(localStorage.getItem("carrito")) || []; /**/ // Obtiene los artículos del carrito del almacenamiento local o crea un array vacío si no hay nada guardado
+        carritoHTML(); /**/ // Llama a la función carritoHTML para mostrar los artículos en el carrito
     })
+
     //Vaciar carrito
-    vaciarCarritoBtn.addEventListener("click", () => {
-        articulosCarrito = [];
-        limpiarHTML();
+    vaciarCarritoBtn.addEventListener("click", () => { /**/ // Escucha el evento click en el botón "Vaciar Carrito"
+        articulosCarrito = []; /**/ // Vacía el array de artículos del carrito
+        limpiarHTML(); /**/ // Llama a la función limpiarHTML para borrar los elementos del carrito en el HTML
     });
 }
 
 // Funciones ****************************************
 
-function agregarCurso (e) {
-    e.preventDefault();
-    // Delegation para agregar-carrito
-    if (e.target.classList.contains('agregar-carrito')) {
+function agregarCurso(e) {
+    e.preventDefault(); /**/ // Previene el comportamiento por defecto del evento click en un enlace
 
-        const curso = e.target.parentElement.parentElement;
-        // Enviamos el curso seleccionado para tomar sus datos
-        console.log(curso);
-        leerDatosCurso(curso);
-        productoAgregado(curso);
+    // Delegation para agregar-carrito
+    if (e.target.classList.contains('agregar-carrito')) { /**/ // Verifica si el elemento clickeado contiene la clase "agregar-carrito"
+        const curso = e.target.parentElement.parentElement; /**/ // Obtiene el elemento padre del padre del elemento clickeado (el curso seleccionado)
+        leerDatosCurso(curso); /**/ // Llama a la función leerDatosCurso para obtener los detalles del curso
+        productoAgregado(curso); /**/ // Llama a la función productoAgregado para mostrar una alerta de producto añadido
     }
 }
-function productoAgregado(curso){
+
+function productoAgregado(curso) {
     //Crear una alerta
     const alert = document.createElement("H4");
     alert.style.cssText = "background-color: blue; color: white; text-align: center;";  /*color producto agregado*/
@@ -54,19 +54,18 @@ function productoAgregado(curso){
     }, 2000);
 }
 
-function eliminarCurso (e) {
-    if (e.target.classList.contains('borrar-curso')) {
-        const cursoId = e.target.getAttribute("data-id");
+function eliminarCurso(e) {
+    if (e.target.classList.contains('borrar-curso')) { /**/ // Verifica si el elemento clickeado contiene la clase "borrar-curso"
+        const cursoId = e.target.getAttribute("data-id"); /**/ // Obtiene el ID del curso a eliminar del atributo data-id
 
         //Elimina del arreglo por el data-id
-        articulosCarrito = articulosCarrito.filter(curso => curso.id !== cursoId);
-        carritoHTML();
+        articulosCarrito = articulosCarrito.filter(curso => curso.id !== cursoId); /**/ // Filtra los artículos del carrito para eliminar el curso con el ID seleccionado
+        carritoHTML(); /**/ // Actualiza la visualización del carrito en el HTML
     }
 }
 
 //Lee el contenido del HTML al que le dimos click y extrae la información del curso.
-function leerDatosCurso (curso) {
-    console.log(curso);
+function leerDatosCurso(curso) {
     //Crear un objeto con el contenido del curso actual
     const infoCurso = {
         imagen: curso.querySelector('img').src,
@@ -75,66 +74,60 @@ function leerDatosCurso (curso) {
         id: curso.querySelector('a').getAttribute('data-id'),
         cantidad: 1
     }
+
     //Revisa si un elemento ya existe en el carrito
     const existe = articulosCarrito.some(curso => curso.id === infoCurso.id);
     if (existe) {
         //Creamos una copia del arreglo
         const cursos = articulosCarrito.map(curso => {
-
             if (curso.id === infoCurso.id) {
                 curso.cantidad++;
                 return curso; // este retorna el objeto actualizado
             } else {
-                return curso;// retorna los que no son duplicados
+                return curso; // retorna los que no son duplicados
             }
         });
-        articulosCarrito = [ ...cursos ];
-
+        articulosCarrito = [...cursos];
     } else {
-        articulosCarrito = [ ...articulosCarrito, infoCurso ];
+        articulosCarrito = [...articulosCarrito, infoCurso];
     }
 
     //Agregar elementos al carrito  
     carritoHTML();
 }
 
-
 // Muestra el carrito de compras en el HTML
-function carritoHTML () {
-
-    limpiarHTML();
+function carritoHTML() {
+    limpiarHTML(); /**/ // Llama a la función limpiarHTML para borrar los elementos del carrito en el HTML
 
     articulosCarrito.forEach(curso => {
-        const row = document.createElement('tr');
+        const row = document.createElement('tr'); /**/ // Crea una nueva fila para cada curso en el carrito se utiliza para definir una celda dentro de una fila 
         row.innerHTML = `
             <td>  
-                <img src="${ curso.imagen }" width=100>
+                <img src="${curso.imagen}" width=100>
             </td>
-            <td>${ curso.titulo }</td>
-            <td>${ curso.precio }</td>
-            <td>${ curso.cantidad } </td>
+            <td>${curso.titulo}</td>   se utiliza para definir una celda dentro de una fila 
+            <td>${curso.precio}</td>
+            <td>${curso.cantidad} </td>
             <td>
-                <a href="#" class="borrar-curso" data-id="${ curso.id }">X</a>
+                <a href="#" class="borrar-curso" data-id="${curso.id}">X</a>
             </td>
         `;
-        contenedorCarrito.appendChild(row);
+        contenedorCarrito.appendChild(row); /**/ // Agrega la fila al tbody del carrito en el HTML
     });
     //Agregar el carrito de compras al storage
-    sincronizarStorage();
+    sincronizarStorage(); /**/ // Llama a la función sincronizarStorage para guardar los artículos del carrito en el almacenamiento local
+}
 
-}
 function sincronizarStorage() {
-    localStorage.setItem("carrito", JSON.stringify(articulosCarrito));
+    localStorage.setItem("carrito", JSON.stringify(articulosCarrito)); /**/ // Convierte el array de artículos del carrito a formato JSON y lo guarda en el almacenamiento local
 }
+
 // Elimina los cursos del tbody
-function limpiarHTML () {
+function limpiarHTML() {
     //forma lenta
     //:contenedorCarrito.innerHTML = '';
-    while (contenedorCarrito.firstChild) {
-        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+    while (contenedorCarrito.firstChild) { /**/ // Mientras haya un primer hijo en el tbody del carrito
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild); /**/ // Elimina ese primer hijo
     }
 }
-
-
-
-
